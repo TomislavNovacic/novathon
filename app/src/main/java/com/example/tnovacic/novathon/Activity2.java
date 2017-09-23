2fbd4a4b42074c691d5e6d634d38be5b19e74814
Danijel T <danijel.turic@gmail.com>
2017-09-23 14:54:21 +0200

    bars

--- app/src/main/java/com/example/tnovacic/novathon/Activity2.java
+++ app/src/main/java/com/example/tnovacic/novathon/Activity2.java
@@ -1,15 +1,18 @@
 package com.example.tnovacic.novathon;
 
 import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class Activity2 extends AppCompatActivity {
 
     LinearLayout[] bars = new LinearLayout[5];
+    FloatingActionButton float_but;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,7 @@ public class Activity2 extends AppCompatActivity {
         bars[2] = (LinearLayout) findViewById(R.id.bills_bar);
         bars[3] = (LinearLayout) findViewById(R.id.fuel_bar);
         bars[4] = (LinearLayout) findViewById(R.id.other_bar);
+        float_but = (FloatingActionButton) findViewById(R.id.float_but);
 
 
         bars[0].setBackgroundColor(Color.parseColor("#3F51B5"));
@@ -28,6 +32,14 @@ public class Activity2 extends AppCompatActivity {
         bars[2].setBackgroundColor(Color.parseColor("#F4511E"));
         bars[3].setBackgroundColor(Color.parseColor("#CDDC39"));
         bars[4].setBackgroundColor(Color.parseColor("#8D6E63"));
+
+        float_but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout.LayoutParams parms = new LinearLayout.LayoutParams((int)(65*0.50),30);
+                bars[2].setLayoutParams(parms);
+            }
+        });
 
     }
 }
