cf89d2e691b03443c891a58f3e4055444278b482
Danijel T <danijel.turic@gmail.com>
2017-09-23 12:55:37 +0200

    nova

--- app/src/main/java/com/example/tnovacic/novathon/MainActivity.java
+++ app/src/main/java/com/example/tnovacic/novathon/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.tnovacic.novathon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText user, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user = (EditText) findViewById(R.id.user_login);
+        password = (EditText) findViewById(R.id.password_login);
+    }
+
+    public void logiranje(View v){
+        JSONpodaci JS = new JSONpodaci();
+
+        if( (user.getText().equals("")) && (password.getText().equals(""))){
+            String zaslanje = JS.pakiranje(user.getText().toString(), password.getText().toString());
+        }
     }
 }
