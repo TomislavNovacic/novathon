88a9e2978e2214ea0d7dfe5d351dd9af558734a1
Danijel T <danijel.turic@gmail.com>
2017-09-23 12:57:29 +0200

    intent je gore

--- app/src/main/java/com/example/tnovacic/novathon/MainActivity.java
+++ app/src/main/java/com/example/tnovacic/novathon/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tnovacic.novathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     EditText user, password;
+    Intent Activity2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         user = (EditText) findViewById(R.id.user_login);
         password = (EditText) findViewById(R.id.password_login);
+        Activity2 = new Intent(this, Activity2.class);
     }
 
     public void logiranje(View v){
